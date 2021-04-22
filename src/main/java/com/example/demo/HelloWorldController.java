33595264930f72fc022d746c9e134187b550f31e
winnieywhsiao <49788032+winnieywhsiao@users.noreply.github.com>
2021-04-22 21:35:48 +0800

    4/22 2nd VERSION -  Controller Annotation

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+// @RequestMapping 注解就是通過它來處理URL的請求，而這個動作相等於 Servlet 中在web.xml的配置
 import org.springframework.web.bind.annotation.RestController;
+// @RestController注解就是表示當下的java是一個控制器(Controller)
 
 @RestController
-public class SpringBootHelloWorld {
+public class HelloWorldController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,7 +15,7 @@ public class SpringBootHelloWorld {
 	
 	@RequestMapping("/")
 	public String hello(){
-		return "Hey, Spring Boot 的 Hello World ! ";
+		return "Hey, This is Spring Boot's Hello World ! ";
 	}
 
 }
