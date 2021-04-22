9ecbcf469588a4067329db5ee9df6fe0a23c0b53
winnieywhsiao <49788032+winnieywhsiao@users.noreply.github.com>
2021-04-22 21:22:54 +0800

    4/22 1st VERSION - Hello World

---
+++ src/main/java/com/example/demo/SpringProject0422Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject0422Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject0422Application.class, args);
+	}
+
+}
